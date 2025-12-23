ae30d5703043aa593b8b69c7a6b5864cc763364a
sanskar-1802 <sanskar18092004@gmail.com>
2025-12-24 00:40:46 +0530

    added

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const { errorHandler } = require('./middleware/errorHandler');
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true,
 }));
 

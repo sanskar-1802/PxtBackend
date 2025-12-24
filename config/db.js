1f5ff3dc248346faf68bb7995d4647c7f7231b58
sanskar-1802 <sanskar18092004@gmail.com>
2025-12-24 16:26:27 +0530

    added

--- config/db.js
+++ config/db.js
@@ -1,13 +1,35 @@
+// const mongoose = require('mongoose');
+// const logger = require('../utils/logger');
+
+// module.exports = async function connectDB() {
+//   const uri = process.env.MONGO_URI || 'mongodb://localhost:27017';
+//   try {
+//     await mongoose.connect(uri, { dbName: 'expenseDB' });
+//     logger.info('MongoDB connected');
+//   } catch (err) {
+//     logger.error('MongoDB connection error', err);
+//     process.exit(1);
+//   }
+// };
+
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 
 module.exports = async function connectDB() {
-  const uri = process.env.MONGO_URI || 'mongodb://localhost:27017';
+  const uri = process.env.MONGO_URI;
+
+  if (!uri) {
+    console.error("❌ MONGO_URI not found");
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(uri, { dbName: 'expenseDB' });
-    logger.info('MongoDB connected');
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 5000
+    });
+    logger.info('✅ MongoDB connected');
   } catch (err) {
-    logger.error('MongoDB connection error', err);
+    logger.error('❌ MongoDB connection error', err);
     process.exit(1);
   }
 };

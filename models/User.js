870c3c98974320f1e1c3d53b7f4b15ea6d6fdd3c
sanskar-1802 <sanskar18092004@gmail.com>
2025-12-29 14:15:53 +0530

    added

--- models/User.js
+++ models/User.js
@@ -9,15 +9,18 @@ const userSchema = new mongoose.Schema({
   currency: { type: String, default: 'USD' },
 }, { timestamps: true });
 
+// Hash password before save
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-userSchema.methods.matchPassword = async function(entered) {
-  return await require('bcrypt').compare(entered, this.password);
+// Compare password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
